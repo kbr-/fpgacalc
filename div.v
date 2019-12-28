335b7dbe497143284f85757b6271d80c13cc9f7d
Kamil Braun <kb346840@students.mimuw.edu.pl>
2019-12-28 17:04:38 +0100

    opt div

--- div.v
+++ div.v
@@ -11,24 +11,19 @@ module div #(parameter BITS = 32) (
     output reg rdy
 );
 
-reg set_rdy;
 initial rdy     = 0;
-initial set_rdy = 0;
 
 reg [clog2(BITS)-1:0] i;
 reg [BITS-1:0] curr_n, curr_d;
-reg [BITS-1:0] last_curr_n, last_curr_d;
 
 reg [BITS-1:0] r_tmp;
-
 always @(posedge clk) begin
-    last_curr_n <= curr_n;
-    last_curr_d <= curr_d;
-
     if (start) begin
+        curr_n <= n;
+        curr_d <= d;
         i <= BITS - 1;
         r <= 0;
-        set_rdy <= 0;
+        rdy <= 0;
     end else if (!rdy) begin
         r_tmp = {r[BITS-2:0], curr_n[i]};
 
@@ -37,18 +32,7 @@ always @(posedge clk) begin
 
         i <= i - 1;
         if (i == 0)
-            set_rdy <= 1;
-    end
-end
-
-always @* begin
-    curr_n = last_curr_n;
-    curr_d = last_curr_d;
-    rdy    = set_rdy;
-    if (start) begin
-        curr_n = n;
-        curr_d = d;
-        rdy = 0;
+            rdy <= 1;
     end
 end
 
